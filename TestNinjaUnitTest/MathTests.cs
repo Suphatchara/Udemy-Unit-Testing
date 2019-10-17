91b6567483887859a081a1ff615ea7cd8edb890e
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-17 20:18:35 +0700

    6-Writing a Simple Unit Test (1.22-2.08)

--- TestNinjaUnitTest/MathTests.cs
+++ TestNinjaUnitTest/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace TestNinjaUnitTest
 {
+    [TestFixture]
     class MathTests
     {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+
+        }
+
     }
 }
