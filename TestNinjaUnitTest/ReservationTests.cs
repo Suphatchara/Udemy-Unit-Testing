035ab590e7804d0a419157608afa96b044bd136a
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-17 18:24:42 +0700

    8-Testing All the Execution Tests (0.39-5.22)

--- TestNinjaUnitTest/ReservationTests.cs
+++ TestNinjaUnitTest/ReservationTests.cs
@@ -20,5 +20,32 @@ namespace TestNinjaUnitTest
             Assert.IsTrue(result);
         }
 
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_Return()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+
+
+        }
+         [TestMethod]
+         public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+
+
+
+        }
+
+
     }
+
 }
