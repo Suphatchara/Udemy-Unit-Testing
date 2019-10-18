03e6a7621af5b91f6342b750e7159bb196175606
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-18 21:08:12 +0700

    2-Testing Strings (0.44-1.27)

--- TestNinjaUnitTest/HtmlFormatterTests.cs
+++ TestNinjaUnitTest/HtmlFormatterTests.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Fundamentals;
 
 namespace TestNinjaUnitTest
 {
-    class HtmlFormatterTests
+    [TestFixture]
+    public class HtmlFormatterTests
     {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+
+        }
+
     }
 }
