a84d61d56cca7c260aa261def20704ce108e7df2
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 17:08:00 +0700

    2-20-Solution-FizzBuzz (1.55-2.59)

--- TestNinjaUnitTest/FizzBuzzTests.cs
+++ TestNinjaUnitTest/FizzBuzzTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace TestNinjaUnitTest
 {
-    class FizzBuzzTests
+    [TestFixture]
+    public class FizzBuzzTests
     {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+        }
     }
 }
