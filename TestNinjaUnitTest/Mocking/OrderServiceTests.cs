35ac8a2fa8e8de89a5debc99faefe3099124b319
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 23:24:16 +0700

    11-Testing the Interaction Between Two Objects (NewClassTest)

---
+++ TestNinjaUnitTest/Mocking/OrderServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinjaUnitTest.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+
+        }
+    }
+}
