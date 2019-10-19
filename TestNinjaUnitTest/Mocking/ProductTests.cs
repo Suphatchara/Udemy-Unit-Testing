dd4c61870530b55971f8df6de672f8c83f083e41
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 23:50:50 +0700

    13-An Example of a Mock Abuse

--- TestNinjaUnitTest/Mocking/ProductTests.cs
+++ TestNinjaUnitTest/Mocking/ProductTests.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinjaUnitTest.Mocking
 {
-    [TestClass]
+    [TestFixture]
     public class ProductTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
         {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
         }
+
     }
 }
