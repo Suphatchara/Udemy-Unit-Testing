acc8c253149cd4f2b18fe5d1dec8a867d4004334
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 03:05:44 +0700

    4-Writing the First Test

--- TestNinjaUnitTest/Mocking/BookingHelperTests.cs
+++ TestNinjaUnitTest/Mocking/BookingHelperTests.cs
@@ -28,14 +28,17 @@ namespace TestNinjaUnitTest.Mocking
                 }
             }.AsQueryable());
 
-            BookingHelper.OverlappingBookingsExist(new Booking
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
             {
                 Id = 2,
-                ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
-                DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
+                ArrivalDate = new DateTime(2017, 1, 10, 14, 0, 0),
+                DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0),
 
 
             }, repository.Object);
+
+            Assert.That(result, Is.Empty);
+
         }
     }
 }
