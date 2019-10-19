60b65e5e3325a1411333a54047e0da3d56873895
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 02:46:20 +0700

    3-Extracting IBooking Repository

--- TestNinja/Mocking/BookingRepository.cs
+++ TestNinja/Mocking/BookingRepository.cs
@@ -8,13 +8,15 @@ namespace TestNinja.Mocking
 {
     public class BookingRepository
     {
-        public IQueryable<Booking> GetActiveBookings()
+        public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
         {
             var unitOfWork = new UnitOfWork();
             var bookings =
                 unitOfWork.Query<Booking>()
                     .Where(
-                        b => b.Id != booking.Id && b.Status != "Cancelled
+                        b => b.Status != "Cancelled");
+            if (excludedBookingId.HasValue)
+                bookings = bookings.Where (b=> b.Id != excludedBookingId.Value);
             return bookings;
 
         }
