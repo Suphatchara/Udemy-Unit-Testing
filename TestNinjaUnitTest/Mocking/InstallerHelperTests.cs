8a2f4293eb6245a538a70871bfdbdf898eb0a166
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 01:30:10 +0700

    6-Testing InstallerHelper (NewClassTests)

---
+++ TestNinjaUnitTest/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinjaUnitTest.Mocking
+{
+    [TestClass]
+    public class InstallerHelperTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
