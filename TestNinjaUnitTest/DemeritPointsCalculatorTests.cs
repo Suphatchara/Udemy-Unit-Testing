d59d4a1ae650a66a2d12d143617f7a19fdf61ec4
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 19:51:10 +0700

    4-Solution-DemeritPointsCalculator

--- TestNinjaUnitTest/DemeritPointsCalculatorTests.cs
+++ TestNinjaUnitTest/DemeritPointsCalculatorTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Fundamentals;
 
 namespace TestNinjaUnitTest
 {
@@ -13,14 +14,25 @@ namespace TestNinjaUnitTest
         [Test]
         public void CalculateDemeritPoints_SpeedIsNegative_ThrowArgumentOurOfRangeException()
         {
+            var calculator = new DemeritPointsCalculator();
 
-        } 
+            calculator.CalculateDemeritPoints(-1);
+
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsOver300_ThrowArgumentOurOfRangeException()
+        {
+
+        }
 
         [Test]
         [TestCase(0, 0)]
         [TestCase(64, 0)]
         [TestCase(65, 0)]
         [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
         public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoint(int speed)
         {
 
