6f7518b353ca426230735dd87c2f88533e759b38
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 01:59:51 +0700

    8-Refactoring EmployeeController

--- TestNinja/Mocking/EmployeeStorage.cs
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -8,6 +8,14 @@ namespace TestNinja.Mocking
 {
     public class EmployeeStorage
     {
+        public void DeleteEmployee(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+
+        }
+       
 
     }
 }
