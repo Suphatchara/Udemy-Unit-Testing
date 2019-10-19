e45d63f570f927693f8901383495a1a3db73936a
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 17:11:19 +0700

    2-20-Solution-FizzBuzz (3.20-4.04)

--- TestNinjaUnitTest/FizzBuzzTests.cs
+++ TestNinjaUnitTest/FizzBuzzTests.cs
@@ -14,5 +14,24 @@ namespace TestNinjaUnitTest
         public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
         {
         }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
+        {
+
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber()
+        {
+
+        }
+
     }
 }
