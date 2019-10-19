8191a5ca337d3b2f425dca551a487100bbf8a842
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 03:17:08 +0700

    5-Refactoring

--- TestNinjaUnitTest/Mocking/BookingHelperTests.cs
+++ TestNinjaUnitTest/Mocking/BookingHelperTests.cs
@@ -11,27 +11,31 @@ namespace TestNinjaUnitTest.Mocking
     [TestFixture]
     public class BookingHelper_OverlappingBookingsExistTests
     {
+        private Booking _booking;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _booking = new Booking
+            {
+                Id = 2,
+                ArrivalDate = ArriveOn(2017, 1, 15),
+                DepartureDate = DepartOn(2017, 1, 20),
+                Reference = "a"
+
+
+            };
+
+        }
+
         [Test]
         public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
-            var repository = new Mock<IBookingRepository>();
-            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
-            {
-                new Booking
-                {
-                    Id = 2,
-                    ArrivalDate = ArriveOn(2017, 1, 15),
-                    DepartureDate = DepartOn(2017, 1, 20),
-                    Reference = "a"
-
-
-                }
-            }.AsQueryable());
-
+          
             var result = BookingHelper.OverlappingBookingsExist(new Booking
             {
                 Id = 1,
-                ArrivalDate = ArriveOn(2017, 1, 1),
+                ArrivalDate = ArriveOn(2017, 1, 10),//Before(_existingBooking.ArrivalDate)
                 DepartureDate = DepartOn(2017, 1, 14),
 
 
