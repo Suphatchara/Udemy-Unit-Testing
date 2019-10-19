0c752d7b010ff34dea693ce282d04a2a54032d3f
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 21:18:54 +0700

    4-Dependency Injection via Method Parameters

--- TestNinjaUnitTest/Mocking/VideoServiceTests.cs
+++ TestNinjaUnitTest/Mocking/VideoServiceTests.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Mocking;
 
 namespace TestNinjaUnitTest.Mocking
 {
+    [TestFixture]
     class VideoServiceTests
     {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase;
+
+
+        }
+
     }
 }
