5922d71ae7e65e848b5444c6976395f46021ce9c
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 02:35:22 +0700

    2-Test Cases

--- TestNinjaUnitTest/Mocking/BookingHelperTests.cs
+++ TestNinjaUnitTest/Mocking/BookingHelperTests.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TestNinjaUnitTest.Mocking
 {
-    class BookingHelperTests
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingsExistTests
     {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
     }
 }
+
