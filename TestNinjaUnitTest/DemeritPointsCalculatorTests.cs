f809e8fee184afae7e625215abd91a71411d0684
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-19 19:38:05 +0700

    4-Solution-DemeritPointsCalculator

---
+++ TestNinjaUnitTest/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinjaUnitTest
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsNegative_ThrowArgumentOurOfRangeEx()
+        {
+
+        } 
+    }
+}
