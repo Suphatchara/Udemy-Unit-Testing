5ffa559e0d2b023035d52fb6e8f6113fae583c68
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 04:03:18 +0700

    8-Writing Additional Tests

--- TestNinjaUnitTest/Mocking/BookingHelperTests.cs
+++ TestNinjaUnitTest/Mocking/BookingHelperTests.cs
@@ -84,6 +84,18 @@ namespace TestNinja.UnitTests.Mocking
         }
 
 
+        [Test]
+        public void BookingStartsInTheMiddleOfAnExistingBookingButFinishesAfter_ReturnExistingBookingsReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = After(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate)
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
 
         private DateTime Before(DateTime dateTime, int days = 1)
         {
