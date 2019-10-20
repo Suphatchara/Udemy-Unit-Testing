0a400aff0d75d2e6fd183591514c026daf88f408
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 17:19:59 +0700

    5- Writing the First Interaction Test

--- TestNinjaUnitTest/Mocking/HousekeeperServiceTests.cs
+++ TestNinjaUnitTest/Mocking/HousekeeperServiceTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Moq;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using TestNinja.Mocking;
@@ -30,7 +31,7 @@ namespace TestNinjaUnitTest.Mocking
                 messageBox.Object);
 
             service.SendStatementEmails(new DateTime(2017, 1, 1));
-
+            statementGenerator.Verify(sg => sg.SaveStatement(1, "b", (new DateTime(2017, 1, 1))));
 
         }
     }
