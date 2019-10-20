af61307275c5023c48f20ff025696efb0b1bbf8a
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-20 17:24:10 +0700

    5- Writing the First Interaction Test

--- TestNinja/Mocking/EmailSender.cs
+++ TestNinja/Mocking/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,4 +47,4 @@ namespace TestNinja.Mocking
         }
     }
 }
-}	}
+
